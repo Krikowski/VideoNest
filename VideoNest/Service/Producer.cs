5437109570e4c4f719984aa5dac38b3653275f4d
Krikowski <debora.w199@gmail.com>
2025-08-22 00:39:26 -0300

    Remove default environment setup and add RabbitMQ connection test
    Removida a dependência do banco de dados para facilitar testes.
    Adicionado teste de conexão RabbitMQ com producer e consumer.

---
+++ VideoNest/Service/Producer.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace VideoNest.Service {
+    public class Producer {
+        public static void SendMessage(string message) {
+            var factory = new ConnectionFactory {
+                HostName = "rabbitmq",
+                UserName = "admin",
+                Password = "admin",
+                Port = 5672,
+                DispatchConsumersAsync = false // importante para simplificar
+            };
+
+            using IConnection connection = factory.CreateConnection(); // versão síncrona ainda existe
+            using IModel channel = connection.CreateModel();            // cria o canal
+
+            channel.QueueDeclare(queue: "minha_fila",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "",
+                                 routingKey: "minha_fila",
+                                 basicProperties: null,
+                                 body: body);
+
+            Console.WriteLine($"Mensagem enviada: {message}");
+        }
+    }
+}
