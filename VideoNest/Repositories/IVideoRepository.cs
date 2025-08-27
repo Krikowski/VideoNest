b2e1b82b6972d077f9ee813d66b7ced748f71dba
Krikowski <debora.w199@gmail.com>
2025-08-27 17:42:00 -0300

    Cria estrutura inicial com conexão ao Postgres
    - Criados DTOs, Repositories e Services

---
+++ VideoNest/Repositories/IVideoRepository.cs
@@ -0,0 +1,8 @@
+﻿using VideoNest.Models;
+
+namespace VideoNest.Repositories {
+    public interface IVideoRepository {
+        Task SaveVideoAsync(VideoDB video);
+        Task<VideoDB> GetVideoByIdAsync(int id);
+    }
+}
